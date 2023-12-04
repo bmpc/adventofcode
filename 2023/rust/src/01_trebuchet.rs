2659cef60fc4739c7f2bd6ddba20634193acb57a
Bruno Conde <brunompconde@gmail.com>
2023-12-04 23:57:10 +0000

    Day 1 complete

---
+++ 2023/rust/src/01_trebuchet.rs
@@ -0,0 +1,71 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io;
+use std::path::Path;
+
+static CONVERT_NUMBERS: bool = true;
+
+static INPUT_FILE: &str = "./01_input.txt";
+static NUMBERS: &[(&str, &str)] = &[("1", "one"), ("2", "two"), ("3", "three"), ("4", "four"), ("5", "five"), ("6", "six"), ("7", "seven"), ("8", "eight"), ("9", "nine")];
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn convert_spelled_numbers_to_digits(text: &str) -> String {
+    let dup = NUMBERS.iter().fold(text.to_owned(), |acc, num| acc.replace(num.1, &(num.1.to_owned() + num.1)));
+
+    NUMBERS.iter().fold(dup, |acc, num| acc.replace(num.1, num.0))
+}
+
+fn get_value(text: &str) -> u8 {
+    let mut first_dig: Option<char> = None;
+    let mut last_dig: Option<char> = None; 
+    
+    for c in text.chars() { 
+        if c.is_digit(10) {
+            match first_dig {
+                Some(_) => last_dig = Some(c),
+                None => {
+                    first_dig = Some(c);
+                    last_dig = Some(c);
+                }
+            }
+        }
+    }
+
+    if let Some(digit1) = first_dig {
+        (digit1.to_digit(10).unwrap() * 10 + last_dig.unwrap().to_digit(10).unwrap()) as u8
+    } else {
+        0
+    }
+ }
+
+fn main() {
+    if let Ok(lines) = read_lines(INPUT_FILE) {
+        let mut sum: u32 = 0;
+        for line in lines {
+            if let Ok(text) = line {
+                let value: u8 = if CONVERT_NUMBERS == true {
+                    let converted_text: String = convert_spelled_numbers_to_digits(&text);
+                    get_value(&converted_text)
+                } else {
+                    get_value(&text)
+                };
+
+                // println!("{} = {}", &text, &value);
+
+                sum += value as u32;
+            }
+        }
+
+        println!("Sum of all calibration values: {}", sum);
+    } else {
+        eprintln!("Could not extract codes from {}", INPUT_FILE);
+    }
+
+}

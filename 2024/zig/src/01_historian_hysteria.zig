c7c545821ec392b5e81d967e1ddaee5005d95db3
Bruno Conde <brunompconde@gmail.com>
2024-12-03 11:50:13 +0000

    2024 Day 1 complete

---
+++ 2024/zig/src/01_historian_hysteria.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+const utils = @import("utils.zig");
+
+const assert = std.debug.assert;
+const Tuple = std.meta.Tuple(&.{ u32, u32 });
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const result = try solution(allocator, "input/01_input.txt");
+
+    std.debug.print("Part 1: {d}\n", .{result[0]});
+    std.debug.print("Part 2: {d}\n", .{result[1]});
+}
+
+fn solution(allocator: std.mem.Allocator, input: []const u8) !Tuple {
+    const content = try utils.readFile(&allocator, input);
+    defer allocator.free(content);
+
+    var lines = std.mem.split(u8, content, "\n");
+
+    var list_a = std.ArrayList(i32).init(allocator);
+    defer list_a.deinit();
+
+    var list_b = std.ArrayList(i32).init(allocator);
+    defer list_b.deinit();
+
+    while (lines.next()) |line| {
+        var pair = std.mem.split(u8, line, "   ");
+        const str_a = pair.next();
+        assert(str_a != null);
+        const str_b = pair.next();
+        assert(str_b != null);
+
+        const a = try std.fmt.parseInt(i32, str_a.?, 10);
+        const b = try std.fmt.parseInt(i32, str_b.?, 10);
+
+        try list_a.append(a);
+        try list_b.append(b);
+    }
+
+    const slice_a = try list_a.toOwnedSlice();
+    defer allocator.free(slice_a);
+    const slice_b = try list_b.toOwnedSlice();
+    defer allocator.free(slice_b);
+
+    std.mem.sort(i32, slice_a, {}, std.sort.asc(i32));
+    std.mem.sort(i32, slice_b, {}, std.sort.asc(i32));
+
+    const count = slice_a.len;
+
+    var total1: u32 = 0;
+    for (0..count) |idx| {
+        total1 += @abs(slice_a[idx] - slice_b[idx]);
+    }
+
+    var total2: u32 = 0;
+    for (slice_a) |a| {
+        var v_count: i32 = 0;
+        for (slice_b) |b| {
+            if (a == b) {
+                v_count += 1;
+            } else if (v_count > 0) {
+                break;
+            }
+        }
+        total2 += @intCast(a * v_count);
+        v_count = 0;
+    }
+
+    return .{ total1, total2 };
+}
+
+test "example" {
+    const result = try solution(std.testing.allocator, "input/01_input_test.txt");
+
+    try std.testing.expectEqual(@as(u32, 11), result[0]);
+    try std.testing.expectEqual(@as(u32, 31), result[1]);
+}
---
+++ 2024/zig/src/utils.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn concatAndReturnBuffer(allocator: *std.mem.Allocator, one: []const u8, two: []const u8) !std.Buffer {
+    var b = try std.Buffer.init(allocator, one);
+    try b.append(two);
+    return b;
+}
+
+pub fn readFile(allocator: *const std.mem.Allocator, file_path: []const u8) ![]u8 {
+    var file = try std.fs.cwd().openFile(file_path, .{});
+    defer file.close();
+
+    const stat = try file.stat();
+    const buff = try file.readToEndAlloc(allocator.*, stat.size);
+    return buff;
+}
